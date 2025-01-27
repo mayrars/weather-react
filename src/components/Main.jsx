ad3a07272f12abd5ce2b491eed81a9b9e37407d5
mayrars <mayra203@gmail.com>
2025-01-27 05:51:56 -0600

    Import current weather

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,10 @@
 import '../styles/components/Main.scss'
+import CurrentWeather from './CurrentWeather'
 const Main = () => {
   return (
-    <div>Main</div>
+    <div>
+      <CurrentWeather />
+    </div>
   )
 }
 
