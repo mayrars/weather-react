9b9fef50baf6cd9e99e1add83e9d7646febd8236
mayrars <mayra203@gmail.com>
2025-01-26 18:46:58 -0600

    import styles and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Weather from "./components/Weather"
+import './styles/components/App.scss'
+import "bootstrap-icons/font/bootstrap-icons.css"
 
 const App = () => {
-  const dark = false
+  const dark = true
   return (
     <div className={`App-${dark ? 'dark' : 'light'}`}>
       <Header />
