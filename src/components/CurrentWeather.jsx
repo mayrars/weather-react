6382e9213a79885524143058c7c399a1a791a5f8
mayrars <mayra203@gmail.com>
2025-01-29 05:53:41 -0600

    created other info

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -5,6 +5,51 @@ const CurrentWeather = () => {
   const {
     cloud_cover, feels_like, humidity, icon_num, precipitation, summary, temperature, uv_index, visibility, wind
   }= data
+  console.log(data);
+  const otherInfoWidgets = [
+    {
+      id:0,
+      icon: 'droplet',
+      name: 'Precipitation',
+      value: Math.round(precipitation.total),
+      unit: "in/h"
+    },
+    {
+      id:1,
+      icon: 'wind',
+      name: 'Wind',
+      value: Math.round(wind.speed),
+      unit: "mph"
+    },
+    {
+      id:2,
+      icon: 'moisture',
+      name: 'Humidity',
+      value: Math.round(humidity),
+      unit: "%"
+    },
+    {
+      id:3,
+      icon:'sunglasses',
+      name: 'UV Index',
+      value: Math.round(uv_index),
+      unit: ''
+    },
+    {
+      id:4,
+      icon: 'cloud-fill',
+      name: 'Cloud Cover',
+      value: Math.round(cloud_cover),
+      unit: "%"
+    },
+    {
+      id:5,
+      icon: 'eye',
+      name: 'Visibility',
+      value: Math.round(visibility),
+      unit: "mi"
+    }
+  ]
   return (
     <div className='CurrentWeather'>
       <div className='temperature'>
@@ -17,7 +62,23 @@ const CurrentWeather = () => {
         </div>
         <div className="summary">{summary}</div>
       </div>
-      <div className='other-infos'></div>
+      <div className='other-infos'>
+        {
+          otherInfoWidgets.map((widget) =>{
+            return <div className='widget' key={widget.id}>
+              <div className="widget-container">
+                <div className="info">
+                  <div className="icon">
+                    <i className={`bi bi-${widget.icon}`}></i>
+                  </div>
+                  <div className="value">{widget.value} {widget.unit}</div>
+                </div>
+                <div className="name">{widget.name}</div>
+              </div>
+            </div>
+          })
+        }
+      </div>
     </div>
   )
 }
