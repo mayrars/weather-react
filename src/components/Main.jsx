d6bd60a0ef99daeea0d491003d6c3ca074f7cc17
mayrars <mayra203@gmail.com>
2025-01-29 05:54:03 -0600

    import forecast

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,12 @@
 import '../styles/components/Main.scss'
 import CurrentWeather from './CurrentWeather'
+import Forecast from './Forecast'
 const Main = () => {
   return (
     <div>
       <CurrentWeather />
+      <Forecast type="hourly" title="HOURLY FORECAST"/>
+      <Forecast type="dail" title="21 days forecast"/>
     </div>
   )
 }
