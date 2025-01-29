b60ff82ea7bfe0bfad5bf315225f454a76f5617d
mayrars <mayra203@gmail.com>
2025-01-29 07:08:24 -0600

    import data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,13 @@
 import '../styles/components/Main.scss'
 import CurrentWeather from './CurrentWeather'
 import Forecast from './Forecast'
+import { getDailyForecast, getHourlyForecast } from '../api'
 const Main = () => {
   return (
     <div>
       <CurrentWeather />
-      <Forecast type="hourly" title="HOURLY FORECAST"/>
-      <Forecast type="dail" title="21 days forecast"/>
+      <Forecast type="hourly" title="HOURLY FORECAST" data={getHourlyForecast()}/>
+      <Forecast type="dail" title="21 days forecast" data={getDailyForecast()}/>
     </div>
   )
 }
