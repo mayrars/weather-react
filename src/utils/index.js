9b0cd38c93aed868d9bd23f66290590df8698b63
mayrars <mayra203@gmail.com>
2025-02-03 11:18:25 -0600

    created feault place

---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+export const DEFAULT_PLACE = {
+    name:"Mexico City",
+    place_id:"mexico-city",
+    adm_area1:"Mexico City",
+    adm_area2:null,
+    country:"Mexico",
+    lat:"19.42847N",
+    lon:"99.12766W",
+    timezone:"America/Mexico_City",
+    type:"settlement"
+}
