1ed1c102e148efba1c44a9683f86e7d58c864a08
mayrars <mayra203@gmail.com>
2025-02-03 11:17:15 -0600

    adding loader

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,22 @@
+import { useContext } from 'react'
 import '../styles/components/Main.scss'
 import CurrentWeather from './CurrentWeather'
 import Forecast from './Forecast'
 import { getDailyForecast, getHourlyForecast } from '../api'
+import WeatherContext from '../context/weather.context'
+import Loader from './Loader'
 const Main = () => {
+  const {loading} = useContext(WeatherContext)
   return (
-    <div>
-      <CurrentWeather />
-      <Forecast type="hourly" title="HOURLY FORECAST" data={getHourlyForecast()}/>
-      <Forecast type="dail" title="21 days forecast" data={getDailyForecast()}/>
+    <div className='Main'>
+      {loading ? 
+        <Loader /> :
+        <>
+          <CurrentWeather />
+          <Forecast type="hourly" title="HOURLY FORECAST" data={getHourlyForecast()}/>
+          <Forecast type="dail" title="21 days forecast" data={getDailyForecast()}/>
+        </>
+      }
     </div>
   )
 }
