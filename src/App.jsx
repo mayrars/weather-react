f9632764ded69114850ef6e4554b4046d638c11c
mayrars <mayra203@gmail.com>
2025-02-02 19:10:20 -0600

    Adding context and import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { useContext } from "react"
 import Header from "./components/Header"
 import Main from "./components/Main"
+import ThemeContext from "./context/theme-context"
 import Weather from "./components/Weather"
 import './styles/components/App.scss'
 import "bootstrap-icons/font/bootstrap-icons.css"
 
 const App = () => {
-  const dark = true
+  const {dark} = useContext(ThemeContext)
   return (
     <div className={`App-${dark ? 'dark' : 'light'}`}>
       <Header />
