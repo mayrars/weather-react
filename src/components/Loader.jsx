526142302f4c029cc28e26f374e7e67a01a94e34
mayrars <mayra203@gmail.com>
2025-02-03 11:17:07 -0600

    adding loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+import "../styles/components/Loader.scss"
+const Loader = () => {
+  return (
+    <div className="Loader">
+        <div className="loader"></div>
+    </div>
+  )
+}
+
+export default Loader
