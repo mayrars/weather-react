745b2e91520607172cce30284c365759b92db6f2
mayrars <mayra203@gmail.com>
2025-02-03 11:17:29 -0600

    change name of place

--- src/components/Place.jsx
+++ src/components/Place.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react"
+import WeatherContext from "../context/weather.context"
+
 const Place = () => {
+  const {place} = useContext(WeatherContext)
   return (
     <div className="Place">
-      <i className="bi bi-geo-alt-fill"></i> <b>Mexico</b>
+      <i className="bi bi-geo-alt-fill"></i> <b>{place.name}</b>,{' '}{place.country}
     </div>
   )
 }
