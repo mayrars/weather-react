5640dcf44b65772c12af58fe984f6a1157c49e2b
mayrars <mayra203@gmail.com>
2025-02-02 19:10:34 -0600

    changed icons

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,13 +1,21 @@
+import { useContext } from 'react'
 import '../styles/components/Settings.scss'
+import ThemeContext from '../context/theme-context'
 const Settings = () => {
+  const {dark, setDark, saveThemeLocalStorage} = useContext(ThemeContext)
+
+  const toggleTheme = () => {
+    setDark((prevDark) => !prevDark)
+    saveThemeLocalStorage(!dark)
+  }
   return (
     <div className="Settings">
       <div className="theme-toggler">
-        <div className="theme-buttons">
-          <div className="light-theme-btn">
+        <div className="theme-buttons" onClick={toggleTheme}>
+          <div className={`light-theme-btn ${dark ? '' : 'active'}`}>
             <i className="bi bi-sun"></i>
           </div>
-          <div className="dark-theme-btn active">
+          <div className={`dark-theme-btn ${dark ? 'active' : ''}`}>
             <i className="bi bi-moon"></i>
           </div>
         </div>
