5f3f1d49c0f0f4bd52fdf7d97499deaaa8ba0258
mayrars <mayra203@gmail.com>
2025-02-05 05:54:24 -0600

    adding units with context

--- src/constants/index.js
+++ src/constants/index.js
@@ -17,3 +17,42 @@ export const MEASURAMENT_SYSTEMS = {
     US: 'us',
     CA: 'ca'
 }
+
+export const UNITS = {
+    metric: {
+      temperature: '°C',
+      precipitation: 'mm/h',
+      wind_speed: 'm/s',
+      visibility: 'km',
+      humidity: '%',
+      uv_index: '',
+      cloud_cover: '%',
+    },
+    us: {
+      temperature: '°F',
+      precipitation: 'in/h',
+      wind_speed: 'mph',
+      visibility: 'mi',
+      humidity: '%',
+      uv_index: '',
+      cloud_cover: '%',
+    },
+    uk: {
+      temperature: '°C',
+      precipitation: 'mm/h',
+      wind_speed: 'mph',
+      visibility: 'mi',
+      humidity: '%',
+      uv_index: '',
+      cloud_cover: '%',
+    },
+    ca: {
+      temperature: '°C',
+      precipitation: 'mm/h',
+      wind_speed: 'km/h',
+      visibility: 'km',
+      humidity: '%',
+      uv_index: '',
+      cloud_cover: '%',
+    },
+  };
