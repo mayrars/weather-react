120dda7d6b8364240551386ae083157c5b5c65c6
mayrars <mayra203@gmail.com>
2025-02-07 07:04:28 -0600

    add property draggable in image

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,6 +1,6 @@
 const WeatherIcon = ({iconNumber, summary}) => {
   return (
-    <img src={`${import.meta.env.VITE_PUBLIC_URL}/dist/set02/big/${iconNumber}.png`} alt={summary} />
+    <img src={`${import.meta.env.VITE_PUBLIC_URL}/dist/set02/big/${iconNumber}.png`} alt={summary} draggable={false}/>
   )
 }
 
